79fa18d1c263a06255cfed9099360f176a178266
AditiJain1001 <128430112+AditiJain1001@users.noreply.github.com>
2023-11-10 09:17:18 -0500

    Add files via upload

---
+++ boiler_plate_code/Code/navigation.js
@@ -0,0 +1,17 @@
+// Mobile hamburger menu funtionality
+
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        //acessabity features
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
